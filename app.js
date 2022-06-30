9c0939f64d656011012e6bf7699fa1a757ccb490
Swapnil Verma <swapnil28verma@gmail.com>
2022-06-30 07:24:52 +0530

    Updated project references from clipboard-manager to scratchy (working title)

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function createWindow () {
 
   mainWindow.loadURL(
     url.format({
-      pathname: path.join(__dirname, `/dist/clipboard-manager/index.html`),
+      pathname: path.join(__dirname, `/dist/scratchy/index.html`),
       protocol: "file:",
       slashes: true
     })
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/clipboard-manager'),
+      dir: require('path').join(__dirname, './coverage/scratchy'),
       subdir: '.',
       reporters: [
         { type: 'html' },
