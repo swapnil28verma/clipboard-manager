472b2d33c58778b680d458d571502e41474b2a40
Swapnil Verma <swapnil28verma@gmail.com>
2022-07-23 16:16:29 +0530

    Added quill text editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTabsModule} from "@angular/material/tabs";
+import { MatTabsModule } from "@angular/material/tabs";
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { QuillModule } from "ngx-quill";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    QuillModule.forRoot({
+      customOptions: [
+        {
+          import: "formats/font",
+          whitelist: [
+            "mirza",
+            "roboto",
+            "aref",
+            "serif",
+            "sansserif",
+            "monospace"
+          ]
+        }
+      ]
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-user-content.js
@@ -0,0 +1,7 @@
+export const mockUserData = {
+  id: '001',
+  label: 'Coding',
+  color: '#bedbb7',
+  // language=HTML
+  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, commodi, ducimus fuga harum iure molestias nostrum nulla quidem sit sunt vero voluptatum? Alias error inventore laborum mollitia neque numquam, vel.</div><div>Alias aperiam aspernatur blanditiis, consectetur culpa debitis dolor doloribus eligendi exercitationem ipsam molestias neque nisi officiis quibusdam quo quod, quos rem repellat sapiente ut! Aliquid delectus est eveniet sit voluptas.</div><div>Amet cum eaque nulla odio quis, quisquam sapiente? Accusamus aliquid aperiam, asperiores aut corporis delectus eius enim error illum inventore labore molestias optio quae quam quibusdam quos sed tempore ullam?</div><div>Accusamus alias assumenda at commodi, deleniti eaque est et ex illo iste laudantium maiores non nulla obcaecati odit optio quaerat quia reprehenderit sapiente sed sit, temporibus vel vero! Optio, vel.</div><div>Consectetur facilis minima sint temporibus. Alias consectetur dolorem eveniet facilis laboriosam natus officia quam, saepe sit! Alias aliquid corporis earum, explicabo ipsa non, porro quam quas qui quisquam sunt vero.'
+}
