7d59cf3db46376f2bc094a0a644dd05a530756d9
Swapnil Verma <swapnil28verma@gmail.com>
2022-11-05 15:33:21 +0530

    Added ElectronService to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {ThemeService} from "./services/theme.service";
-import {indigo} from "./utils/themes/theme";
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from "./services/theme.service";
+import { indigo } from "./utils/themes/theme";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
     public title = "Clipboard-app";
-    constructor(private themeService: ThemeService) {}
+
+    constructor(private themeService: ThemeService) {
+    }
 
     ngOnInit(): void {
         this.themeService.setActiveTheme(indigo);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,41 +7,48 @@ import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from "@angular/material/tabs";
 import { WorkspaceComponent } from './components/workspace/workspace.component';
-import {QuillModule} from "ngx-quill";
-import {ScratchyCoreService} from "./services/scratchy-core.service";
-import {FormsModule} from "@angular/forms";
-import {TabViewModule} from "primeng/tabview";
+import { QuillModule } from "ngx-quill";
+import { ScratchyCoreService } from "./services/scratchy-core.service";
+import { FormsModule } from "@angular/forms";
+import { TabViewModule } from "primeng/tabview";
+import { TabViewComponent } from "./components/utils/tab-view/tab-view.component";
+import { TabPanelComponent } from './components/utils/tab-view/tab-panel/tab-panel.component';
+import { ElectronCoreService } from "./services/electron-core.service";
+import { ThemeService } from "./services/theme.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    WorkspaceComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatTabsModule,
-    TabViewModule,
-    QuillModule.forRoot({
-      customOptions: [
-        {
-          import: "formats/font",
-          whitelist: [
-            "mirza",
-            "roboto",
-            "aref",
-            "serif",
-            "sansserif",
-            "monospace"
-          ]
-        }
-      ]
-    }),
-    FormsModule,
-  ],
-  providers: [ScratchyCoreService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        WorkspaceComponent,
+        TabViewComponent,
+        TabPanelComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        TabViewModule,
+        QuillModule.forRoot({
+            customOptions: [
+                {
+                    import: "formats/font",
+                    whitelist: [
+                        "mirza",
+                        "roboto",
+                        "aref",
+                        "serif",
+                        "sansserif",
+                        "monospace"
+                    ]
+                }
+            ]
+        }),
+        FormsModule,
+    ],
+    providers: [ScratchyCoreService, ElectronCoreService, ThemeService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
